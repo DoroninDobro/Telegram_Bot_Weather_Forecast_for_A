278ebd15f37061f7335b8b297edd2b33adb21bf2
Doronin Vovka <Doronindobro@gmail.com>
2020-02-02 00:50:03 +0300

    before add buttons

--- Forecast_Weather_for_A.py
+++ Forecast_Weather_for_A.py
@@ -1,8 +1,17 @@
 import pyowm
 import telebot
+from config import TOKEN, OWM_TOKEN
+from random import randint
+from telebot import types
 
-bot = telebot.TeleBot("880795081:AAGrql1vXt1644l8CBoLtDdnU_R1kUzo8Wg")
-owm = pyowm.OWM('8d4d7111350ee99f5b0607450670f05c', language = "ru")
+bot = telebot.TeleBot(TOKEN)
+owm = pyowm.OWM(OWM_TOKEN, language = "ru")
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    sti = open('hello.jpeg', 'rb')
+    bot.send_sticker(message.chat.id, sti)
+    bot.send_message(message.chat.id, "Привет!\nЯ -твой личный предсказатель! \nЫЫЫ! =))")
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
@@ -24,9 +33,18 @@ def send_echo(message):
         answer += 'Ну не жарко, захвати с собой что-то для утепления, особенно вечером. 4-1 <3 \n'
     else:
         answer += 'Ммм, неплохо, одевайся как хочешь, самое время показать всем свою красоту! =)) \n'
-
     answer += 'Зато на Бали сейчас ' + str(temp_Bali) + '! И манго!'
 
+    if 'пасмурно' in answer or 'дождь' in answer or 'ливень' in answer:
+        x_r = randint(1, 7)
+        name = f'rain{x_r}.jpeg'
+        stik = open(name, 'rb')
+        bot.send_sticker(message.chat.id, stik)
+    if 'солнечно' in answer or 'солнце' in answer: 
+        sti2 = open('sun.jpeg', 'rb')
+        bot.send_sticker(message.chat.id, sti2)
+        bot.send_message(message.chat.id, 'Ну наконец-то! Бросай все и беги за витамином D!')
+        answer = "Температура сейчас " + str(temp_SP) + '\n\n'
     bot.send_message(message.chat.id, answer)
 
 bot.polling(none_stop=True)
